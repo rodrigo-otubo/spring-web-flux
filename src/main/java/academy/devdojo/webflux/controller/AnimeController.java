e441f19fd0d8a77165c887cc38194fdd8d4e942b
Rodrigo Otubo <rodrigo.otubo@gmail.com>
2021-06-06 21:03:29 -0300

    Class 11 until 20, Tests and CRUD

--- src/main/java/academy/devdojo/webflux/controller/AnimeController.java
+++ src/main/java/academy/devdojo/webflux/controller/AnimeController.java
@@ -4,13 +4,12 @@ import academy.devdojo.webflux.domain.Anime;
 import academy.devdojo.webflux.service.AnimeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RestController
@@ -24,13 +23,25 @@ public class AnimeController {
     }
 
     @GetMapping(path = "{id}")
-    public Mono<Anime> listAll(@PathVariable int id){
-        return animeService.findById(id)
-                .switchIfEmpty(monoResponseStatusNotFoundException())
-                .log();
+    public Mono<Anime> findById(@PathVariable int id){
+        return animeService.findById(id);
     }
 
-    public <T> Mono <T> monoResponseStatusNotFoundException(){
-        return Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Anime not found"));
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<Anime> save(@Valid @RequestBody Anime anime){
+        return animeService.save(anime);
+    }
+
+    @PutMapping(path = "{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public Mono<Void> update(@PathVariable int id, @Valid @RequestBody Anime anime){
+        return animeService.update(anime.withId(id));
+    }
+
+    @DeleteMapping(path = "{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public Mono<Void> delete(@PathVariable int id){
+        return animeService.delete(id);
     }
 }
--- src/main/java/academy/devdojo/webflux/service/AnimeService.java
+++ src/main/java/academy/devdojo/webflux/service/AnimeService.java
@@ -3,7 +3,9 @@ package academy.devdojo.webflux.service;
 import academy.devdojo.webflux.domain.Anime;
 import academy.devdojo.webflux.repository.AnimeRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -18,6 +20,25 @@ public class AnimeService {
     }
 
     public Mono<Anime> findById(int id){
-        return  animeRepository.findById(id);
+        return  animeRepository.findById(id).switchIfEmpty(monoResponseStatusNotFoundException());
+    }
+
+    public <T> Mono<T> monoResponseStatusNotFoundException() {
+        return Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Anime not found"));
+    }
+
+    public Mono<Anime> save(Anime anime) {
+        return animeRepository.save(anime);
+    }
+
+    public Mono<Void> update(Anime anime){
+        return findById(anime.getId())
+                .flatMap(animeRepository::save)
+                .then();
+    }
+
+    public Mono<Void> delete(int id) {
+        return findById(id)
+                .flatMap(animeRepository::delete);
     }
 }
